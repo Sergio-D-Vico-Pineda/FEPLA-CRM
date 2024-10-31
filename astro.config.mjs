ea6b78741642c06b547b7afc640775f9e4b5425a
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-10-31 18:11:12 +0100

    creation conecction and table

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,48 @@
 import { defineConfig } from 'astro/config';
-
+import { createClient } from '@libsql/client/web';
 import tailwind from '@astrojs/tailwind';
+import dotenv from 'dotenv';
 
-// https://astro.build/config
+function log(text) {
+  console.log('SERVER: ' + text);
+}
+
+dotenv.config();
+
 export default defineConfig({
   integrations: [tailwind()]
 });
+
+export const tursodb = createClient({
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN
+});
+
+await tursodb.execute(`
+  CREATE TABLE IF NOT EXISTS empresa (
+  empresaId INTEGER PRIMARY KEY,
+  email VARCHAR,
+  cif VARCHAR,
+  nombre VARCHAR,
+  telefono VARCHAR,
+  disponibilidad VARCHAR,
+  comentarios VARCHAR,
+  fechaRegistro TIMESTAMP
+);`)
+
+await tursodb.execute(`
+  CREATE TABLE IF NOT EXISTS alumno (
+  alumnoId INTEGER PRIMARY KEY,
+  email VARCHAR,
+  dni VARCHAR,
+  nie VARCHAR,
+  nombre VARCHAR,
+  telefono VARCHAR,
+  cursoActual VARCHAR,
+  comentarios VARCHAR,
+  fechaRegistro TIMESTAMP
+);`)
+
+// https://astro.build/config
+
+log('DB created')
