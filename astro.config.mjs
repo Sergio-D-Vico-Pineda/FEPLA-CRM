e29b0a4a7159446f7bfcd6d56999d56a863e141d
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-11-05 11:34:01 +0100

    v0.1.1: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,40 @@
-import { defineConfig } from 'astro/config';
-import { createClient } from '@libsql/client/web';
+import
+{
+    defineConfig
+}
+from 'astro/config';
+import
+{
+    createClient
+}
+from '@libsql/client/web';
 import tailwind from '@astrojs/tailwind';
 import dotenv from 'dotenv';
 
-function log(text) {
-  console.log('SERVER: ' + text);
+function log(text)
+{
+    console.log('SERVER: ' + text);
 }
 
 dotenv.config();
 
-export default defineConfig({
-  integrations: [tailwind()]
+export default defineConfig(
+{
+    integrations: [tailwind()]
 });
 
-export const tursodb = createClient({
-  url: process.env.TURSO_DATABASE_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN
+export const tursodb = createClient(
+{
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN
 });
 
 await tursodb.execute(`
   CREATE TABLE IF NOT EXISTS empresa (
   empresaId INTEGER PRIMARY KEY,
+  nombre VARCHAR,
   email VARCHAR,
   cif VARCHAR,
-  nombre VARCHAR,
   telefono VARCHAR,
   disponibilidad VARCHAR,
   comentarios VARCHAR,
