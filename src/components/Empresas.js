3bf287e6970917df7f3901ad474df9ca9481cd21
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-11-21 19:04:33 +0100

    empresas random para probar

---
+++ src/components/Empresas.js
@@ -0,0 +1,17 @@
+export const rows = Array.from(
+{
+    length: 9
+}, (_, i) =>
+{
+    return {
+        id: i + 1,
+        nombre: `Empresa ${i + 1}`,
+        nombre_oficial: `Empresa oficial ${i + 1}`,
+        direccion: `Calle ${i + 1}, 1, 46001 Valencia`,
+        cif: `B12345678${i + 1}`,
+        sitio_web: `https://empresa${i + 1}.es`,
+        sector: `Sector ${i + 1}`,
+        tecnologias: `tecnologia ${i + 1}`,
+        comentarios: `comentarios ${i + 1}`,
+    };
+});
