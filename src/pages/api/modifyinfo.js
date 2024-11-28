ebc5249d6a5cf95e47c22612601bd5a7a8f32682
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-11-28 16:10:42 +0100

    api to modify

--- src/pages/api/modifyinfo.js
+++ src/pages/api/modifyinfo.js
@@ -24,19 +24,76 @@ export async function GET()
     );
 }
 
-export async function POST()
+export async function POST(
 {
+    request
+})
+{
+    const data = await request.json();
+    console.log(data)
 
-
-    return new Response(JSON.stringify(
+    if (!data.nombre)
     {
-        message: "SERVER: Datos actualizados correctamente."
-    }),
-    {
-        status: 200,
-        headers:
+        return new Response(JSON.stringify(
         {
-            'Content-Type': 'application/json'
-        }
-    });
+            message: "SERVER: Error al recibir los datos. Nombre vacio."
+        }),
+        {
+            status: 500,
+            headers:
+            {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
+    try
+    {
+        let resp = await tursodb.execute(
+        {
+            sql: `UPDATE empresa 
+            SET nombre = $nombre, nombre_oficial = $nombre_oficial, direccion = $direccion, cif = $cif, sitio_web = $sitio_web, sector = $sector, tecnologias = $tecnologias, comentarios = $comentarios, fecha_actualizacion = $fecha_actualizacion
+            WHERE empresa_id = $empresa_id`,
+            args:
+            {
+                empresa_id: data.empresa_id,
+                nombre: data.nombre,
+                nombre_oficial: data.nombre_oficial,
+                direccion: data.direccion,
+                cif: data.cif,
+                sitio_web: data.sitio_web,
+                sector: data.sector,
+                tecnologias: data.tecnologias,
+                comentarios: data.comentarios,
+                fecha_actualizacion: formatDate(new Date()),
+            }
+        });
+
+        return new Response(JSON.stringify(
+        {
+            message: `SERVER: Datos actualizados correctamente. ${resp.rowsAffected}`
+        }),
+        {
+            status: 200,
+            headers:
+            {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+    catch (error)
+    {
+        console.log(error)
+        return new Response(JSON.stringify(
+        {
+            message: "SERVER: Error al actualizar los datos."
+        }),
+        {
+            status: 500,
+            headers:
+            {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
 }
