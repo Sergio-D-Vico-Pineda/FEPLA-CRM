22ca0d50908f5ebaa9165cddb94fc81ea7df712f
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-12-07 13:37:02 +0100

    switch api empresa

---
+++ src/pages/api/empresa/switch.js
@@ -0,0 +1,41 @@
+import
+{
+    tursodb
+}
+from "../../../../astro.config.mjs";
+
+export async function GET()
+{
+    return new Response(
+        `Api to switch on/off a company.`
+    );
+}
+
+export async function POST(
+{
+    request
+})
+{
+    const data = await request.json();
+
+    let resp = await tursodb.execute(
+    {
+        sql: `UPDATE empresa SET activo = $activo WHERE empresa_id = $empresa_id;`,
+        args:
+        {
+            empresa_id: data.empresa_id,
+            activo: data.activo
+        }
+    });
+    return new Response(JSON.stringify(
+    {
+        message: data.activo ? "Empresa activada." : "Empresa desactivada."
+    }),
+    {
+        status: 200,
+        headers:
+        {
+            "Content-Type": "application/json",
+        },
+    });
+}
