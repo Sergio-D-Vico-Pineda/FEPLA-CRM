3c1953ead2c54e1eac746550e9aa80a7609b6d2a
Scarpy19 <nootravez19@gmail.com>
2024-12-10 11:43:53 +0100

    i think grupo pages is done

--- src/pages/api/grupo/modify.js
+++ src/pages/api/grupo/modify.js
@@ -1,90 +1,78 @@
 import { formatDate } from "../funcs.js";
 
-import
-{
+import {
     tursodb
 }
-from "@r/astro.config.mjs";
+    from "@r/astro.config.mjs";
 
-export async function GET()
-{
+export async function GET() {
     return new Response(
         `Api to modify information of a group.`
     );
 }
 
 export async function POST(
-{
-    request
-})
-{
+    {
+        request
+    }) {
     const data = await request.json();
     console.log(data)
 
-    if (!data.nombre)
-    {
+    if (!data.nombre) {
         return new Response(JSON.stringify(
-        {
-            message: "SERVER: Error al recibir los datos. Nombre vacio."
-        }),
-        {
-            status: 500,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: "SERVER: Error al recibir los datos. Nombre vacio."
+            }),
+            {
+                status: 500,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
 
-    try
-    {
+    try {
         let resp = await tursodb.execute(
-        {
-            sql: `UPDATE alumno 
-                SET nombre = $nombre, apellidos = $apellidos, telefono = $telefono, fecha_nacimiento = $fecha_nacimiento, email = $email, nif = $nif, nia = $nia, nuss = $nuss, comentarios = $comentarios, fecha_actualizacion = $fecha_actualizacion
-                WHERE alumno_id = $alumno_id`,
-            args:
             {
-                alumno_id: data.alumno_id,
-                nombre: data.nombre,
-                apellidos: data.apellidos,
-                telefono: data.telefono,
-                fecha_nacimiento: data.fecha_nacimiento,
-                email: data.email,
-                nif: data.nif,
-                nia: data.nia,
-                nuss: data.nuss,
-                comentarios: data.comentarios,
-                fecha_actualizacion: formatDate(new Date()),
-            }
-
-        });
+                sql: `UPDATE grupo 
+                SET nombre = $nombre, descripcion = $descripcion, curso = $curso, comentarios = $comentarios, fecha_actualizacion = $fecha_actualizacion
+                WHERE grupo_id = $grupo_id`,
+                args:
+                {
+                    grupo_id: data.grupo_id,
+                    nombre: data.nombre,
+                    descripcion: data.descripcion,
+                    curso: data.curso,
+                    comentarios: data.comentarios,
+                    fecha_actualizacion: formatDate(new Date()),
+                }
+            });
 
         return new Response(JSON.stringify(
-        {
-            message: `SERVER: Datos actualizados correctamente. ${resp.rowsAffected}`
-        }),
-        {
-            status: 200,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: `SERVER: Datos actualizados correctamente. ${resp.rowsAffected}`
+            }),
+            {
+                status: 200,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
-    catch (error)
-    {
+    catch (error) {
         console.log(error)
         return new Response(JSON.stringify(
-        {
-            message: "SERVER: Error al actualizar los datos."
-        }),
-        {
-            status: 500,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: "SERVER: Error al actualizar los datos."
+            }),
+            {
+                status: 500,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
 }
