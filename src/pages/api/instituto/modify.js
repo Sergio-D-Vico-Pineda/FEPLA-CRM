eba41f257afa1d9270c844f6f24db942ce7398c3
Scarpy19 <nootravez19@gmail.com>
2024-12-10 12:35:05 +0100

    instituto pages and apis

--- src/pages/api/instituto/modify.js
+++ src/pages/api/instituto/modify.js
@@ -1,90 +1,84 @@
 import { formatDate } from "../funcs.js";
 
-import
-{
-    tursodb
+import {
+tursodb
 }
-from "@r/astro.config.mjs";
+    from "@r/astro.config.mjs";
 
-export async function GET()
-{
+export async function GET() {
     return new Response(
         `Api to modify information of an highschool.`
     );
 }
 
 export async function POST(
-{
-    request
-})
-{
+    {
+        request
+    }) {
     const data = await request.json();
     console.log(data)
 
-    if (!data.nombre)
-    {
+    if (!data.nombre) {
         return new Response(JSON.stringify(
-        {
-            message: "SERVER: Error al recibir los datos. Nombre vacio."
-        }),
-        {
-            status: 500,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: "SERVER: Error al recibir los datos. Nombre vacio."
+            }),
+            {
+                status: 500,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
 
-    try
-    {
+    try {
         let resp = await tursodb.execute(
-        {
-            sql: `UPDATE alumno 
+            {
+                sql: `UPDATE alumno 
                 SET nombre = $nombre, apellidos = $apellidos, telefono = $telefono, fecha_nacimiento = $fecha_nacimiento, email = $email, nif = $nif, nia = $nia, nuss = $nuss, comentarios = $comentarios, fecha_actualizacion = $fecha_actualizacion
                 WHERE alumno_id = $alumno_id`,
-            args:
-            {
-                alumno_id: data.alumno_id,
-                nombre: data.nombre,
-                apellidos: data.apellidos,
-                telefono: data.telefono,
-                fecha_nacimiento: data.fecha_nacimiento,
-                email: data.email,
-                nif: data.nif,
-                nia: data.nia,
-                nuss: data.nuss,
-                comentarios: data.comentarios,
-                fecha_actualizacion: formatDate(new Date()),
-            }
+                args:
+                {
+                    alumno_id: data.alumno_id,
+                    nombre: data.nombre,
+                    apellidos: data.apellidos,
+                    telefono: data.telefono,
+                    fecha_nacimiento: data.fecha_nacimiento,
+                    email: data.email,
+                    nif: data.nif,
+                    nia: data.nia,
+                    nuss: data.nuss,
+                    comentarios: data.comentarios,
+                    fecha_actualizacion: formatDate(new Date()),
+                }
 
-        });
+            });
 
         return new Response(JSON.stringify(
-        {
-            message: `SERVER: Datos actualizados correctamente. ${resp.rowsAffected}`
-        }),
-        {
-            status: 200,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: `SERVER: Datos actualizados correctamente. ${resp.rowsAffected}`
+            }),
+            {
+                status: 200,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
-    catch (error)
-    {
+    catch (error) {
         console.log(error)
         return new Response(JSON.stringify(
-        {
-            message: "SERVER: Error al actualizar los datos."
-        }),
-        {
-            status: 500,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: "SERVER: Error al actualizar los datos."
+            }),
+            {
+                status: 500,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
 }
--- src/pages/api/instituto/new.js
+++ src/pages/api/instituto/new.js
@@ -1,90 +1,79 @@
 import { formatDate } from "../funcs.js";
 
-import
-{
+import {
     tursodb
 }
-from "@r/astro.config.mjs";
+    from "@r/astro.config.mjs";
 
-export async function GET()
-{
+export async function GET() {
     return new Response(
         `Api to add a new highschool.`
     );
 }
 
 export async function POST(
-{
-    request
-})
-{
+    {
+        request
+    }) {
     const data = await request.json();
 
-    if (!data.nombre)
-    {
+    if (!data.nombre) {
         return new Response(JSON.stringify(
-        {
-            message: "SERVER: Error al recibir los datos. Nombre vacio."
-        }),
-        {
-            status: 500,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: "SERVER: Error al recibir los datos. Nombre vacio."
+            }),
+            {
+                status: 500,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
-    try
-    {
+    try {
         let resp = await tursodb.execute(
-        {
-            sql: `INSERT INTO alumno(nombre, apellidos, telefono, fecha_nacimiento, email, nif, nia, nuss, comentarios, fecha_creacion, activo, grupo_id, instituto_id)
-        VALUES($nombre, $apellidos, $telefono, $fecha_nacimiento, $email, $nif, $nia, $nuss, $comentarios, $fecha_creacion, $activo, $grupo_id, $instituto_id);`,
-
-            args:
             {
-                nombre: data.nombre,
-                apellidos: data.apellidos,
-                telefono: data.telefono,
-                fecha_nacimiento: data.fecha_nacimiento,
-                email: data.email,
-                nif: data.nif,
-                nia: data.nia,
-                nuss: data.nuss,
-                comentarios: data.comentarios,
-                fecha_creacion: formatDate(new Date()),
-                activo: 1,
-                grupo_id: data.grupo_id,
-                instituto_id: data.instituto_id
-            }
-        });
+                sql: `INSERT INTO instituto(nombre, descripcion, direccion, sitio_web, telefono, comentarios, fecha_creacion)
+        VALUES($nombre, $descripcion, $direccion, $sitio_web, $telefono, $comentarios, $fecha_creacion);`,
+
+                args:
+                {
+                    nombre: data.nombre,
+                    descripcion: data.descripcion,
+                    direccion: data.direccion,
+                    sitio_web: data.sitio_web,
+                    telefono: data.telefono,
+                    comentarios: data.comentarios,
+                    fecha_creacion: formatDate(new Date())
+                }
+
+            });
 
         console.log(resp);
 
         return new Response(JSON.stringify(
-        {
-            message: `SERVER: Datos recibidos correctamente. ${resp.rowsAffected}`
-        }),
-        {
-            status: 200,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: `SERVER: Datos recibidos correctamente. ${resp.rowsAffected}`
+            }),
+            {
+                status: 200,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
-    catch (error)
-    {
+    catch (error) {
         return new Response(JSON.stringify(
-        {
-            message: `SERVER: Error al recibir los datos. ${error}`
-        }),
-        {
-            status: 500,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: `SERVER: Error al recibir los datos. ${error}`
+            }),
+            {
+                status: 500,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
 }
