f54be6cf2c5f94cd63b0ee40098413a156234a04
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-12-18 20:30:45 +0100

    option without

--- src/pages/api/alumno/new.js
+++ src/pages/api/alumno/new.js
@@ -1,90 +1,88 @@
-import { formatDate } from "../funcs.js";
+import { formatDate, extractNumber } from "../funcs.js";
 
-import
-{
-    tursodb
+import {
+tursodb
 }
-from "@r/astro.config.mjs";
+    from "@r/astro.config.mjs";
 
-export async function GET()
-{
+export async function GET() {
     return new Response(
         `Api to add a new student.`
     );
 }
 
 export async function POST(
-{
-    request
-})
-{
+    {
+        request
+    }) {
     const data = await request.json();
 
-    if (!data.nombre)
-    {
+    if (!data.nombre) {
         return new Response(JSON.stringify(
-        {
-            message: "SERVER: Error al recibir los datos. Nombre vacio."
-        }),
-        {
-            status: 500,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: "SERVER: Error al recibir los datos. Nombre vacio."
+            }),
+            {
+                status: 500,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
-    try
-    {
+
+    let grupo_id = extractNumber(data.grupo_id);
+    let instituto_id = extractNumber(data.instituto_id);
+
+    try {
         let resp = await tursodb.execute(
-        {
-            sql: `INSERT INTO alumno(nombre, apellidos, telefono, fecha_nacimiento, email, nif, nia, nuss, comentarios, fecha_creacion, activo, grupo_id, instituto_id)
+            {
+                sql: `INSERT INTO alumno(nombre, apellidos, telefono, fecha_nacimiento, email, nif, nia, nuss, comentarios, fecha_creacion, activo, grupo_id, instituto_id)
         VALUES($nombre, $apellidos, $telefono, $fecha_nacimiento, $email, $nif, $nia, $nuss, $comentarios, $fecha_creacion, $activo, $grupo_id, $instituto_id);`,
 
-            args:
-            {
-                nombre: data.nombre,
-                apellidos: data.apellidos,
-                telefono: data.telefono,
-                fecha_nacimiento: data.fecha_nacimiento,
-                email: data.email,
-                nif: data.nif,
-                nia: data.nia,
-                nuss: data.nuss,
-                comentarios: data.comentarios,
-                fecha_creacion: formatDate(new Date()),
-                activo: 1,
-                grupo_id: data.grupo_id,
-                instituto_id: data.instituto_id
-            }
-        });
+                args:
+                {
+                    nombre: data.nombre,
+                    apellidos: data.apellidos,
+                    telefono: data.telefono,
+                    fecha_nacimiento: data.fecha_nacimiento,
+                    email: data.email,
+                    nif: data.nif,
+                    nia: data.nia,
+                    nuss: data.nuss,
+                    comentarios: data.comentarios,
+                    fecha_creacion: formatDate(new Date()),
+                    activo: 1,
+                    grupo_id: grupo_id,
+                    instituto_id: instituto_id
+                }
+            });
 
         console.log(resp);
 
         return new Response(JSON.stringify(
-        {
-            message: `SERVER: Datos recibidos correctamente. ${resp.rowsAffected}`
-        }),
-        {
-            status: 200,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: `SERVER: Datos recibidos correctamente. ${resp.rowsAffected}`
+            }),
+            {
+                status: 200,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
-    catch (error)
-    {
+    catch (error) {
         return new Response(JSON.stringify(
-        {
-            message: `SERVER: Error al recibir los datos. ${error}`
-        }),
-        {
-            status: 500,
-            headers:
             {
-                'Content-Type': 'application/json'
-            }
-        });
+                message: `SERVER: Error al recibir los datos. ${error}`
+            }),
+            {
+                status: 500,
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            });
     }
 }
--- src/pages/api/funcs.js
+++ src/pages/api/funcs.js
@@ -1,4 +1,4 @@
-export function formatDate(date) {
+function formatDate(date) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Mes (0-11) por eso +1
     const day = String(date.getDate()).padStart(2, '0'); // Día del mes (1-31)
@@ -9,3 +9,10 @@ export function formatDate(date) {
 
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
+
+function extractNumber(str) {
+    const match = str.match(/\((\d+)\)/); // Busca un número entre paréntesis
+    return match ? parseInt(match[1], 10) : null; // Devuelve el número o null si no hay coincidencia
+}
+
+export { formatDate, extractNumber };
